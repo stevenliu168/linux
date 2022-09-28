4a4497eba5dfc59e90352b4587e1f4955d16995e
stevenliu168 <51689495+stevenliu168@users.noreply.github.com>
2022-09-28 17:54:56 +0800

    Update empty3.sh

--- Shell script/empty3.sh
+++ Shell script/empty3.sh
@@ -1,1 +1,31 @@
-g++ -o hel.out helloworld.cpp
+#restart_container.sh
+
+cd .
+pwd
+
+./android11_kbox.sh restart $1 $2
+
+sleep 5
+
+ip=127.0.0.1
+start_num=$((8500+$1))
+echo $start_num
+stop_num=$((8500+$2))
+echo $stop_num
+
+/usr/bin/adb devices
+for port in $(seq ${start_num} ${stop_num})
+do
+{
+        adb connect ${ip}:${port}
+        sleep 3
+        adb -s ${ip}:${port} root
+        sleep 3
+        adb connect ${ip}:${port}
+        sleep 3
+		adb -s ${ip}:${port} shell whoami
+}
+
+done
+
+
