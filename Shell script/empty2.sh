4d827ec7187a22076421d9b48f9538d2633086a0
stevenliu168 <51689495+stevenliu168@users.noreply.github.com>
2022-09-28 17:54:25 +0800

    Update empty2.sh

--- Shell script/empty2.sh
+++ Shell script/empty2.sh
@@ -1,1 +1,30 @@
-g++ -o hel.out helloworld.cpp
+#adb-push-build-prop.sh
+
+cd .
+pwd
+
+sleep 5
+
+ip=127.0.0.1
+start_num=$((8500+$1))
+echo $start_num
+stop_num=$((8500+$2))
+echo $stop_num
+
+/usr/bin/adb devices
+for port in $(seq ${start_num} ${stop_num})
+do
+{
+        #adb connect ${ip}:${port}
+        #sleep 3
+        #adb -s ${ip}:${port} root
+        #sleep 3
+        #adb connect ${ip}:${port}
+        #sleep 3
+        adb -s ${ip}:${port} push ./build.prop /vendor
+        sleep 2
+}
+
+done
+
+
