78ce8f1853d537577a8a076544e6393c244ae771
stevenliu168 <51689495+stevenliu168@users.noreply.github.com>
2022-09-28 17:56:13 +0800

    Update empty7.sh

--- Shell script/empty7.sh
+++ Shell script/empty7.sh
@@ -1,1 +1,16 @@
-g++ -o hel.out helloworld.cpp
+#!/bin/bash
+
+#query-system_server.sh
+
+start_num=$1
+stop_num=$2
+for num in $(seq ${start_num} ${stop_num})
+do
+{
+	CONTAINER_NAME="kbox_$num"
+	echo ${CONTAINER_NAME}:
+    docker exec -it ${CONTAINER_NAME} ps -A | grep system_server
+	sleep 0.5
+}
+
+done
